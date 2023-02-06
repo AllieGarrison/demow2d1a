8772eca1e20381d1cc3d22199d7020493253b203
Kevin Ernest Long <kevinelong@gmail.com>
2023-02-06 21:34:51 +0000

    store

--- main.py
+++ main.py
@@ -130,8 +130,89 @@ THIS MULTIPLIES TWO NUMBERS A AND B
 # power = lambda x: x * x
 
 
-def power(x):
-    return x * x
+# def power(x):
+#     return x * x
+#
+#
+# print(power(3))
+
+"""
+Online shopping is a form of electronic commerce which allows consumers to directly buy goods or services from
+ a seller over the Internet using a web browser or a mobile app. Consumers find a product of interest by visiting
+  the website of the retailer directly or by searching among alternative vendors using a shopping search engine, 
+  which displays the same product's availability and pricing at different e-retailers. As of 2020, customers can 
+  shop online using a range of different computers and devices, including desktop computers, laptops, tablet 
+  computers and smartphones.
+
+An online shop evokes the physical analogy of buying products or services at a regular "bricks-and-mortar"
+ retailer or shopping center; the process is called business-to-consumer (B2C) online shopping. When an online 
+ store is set up to enable businesses to buy from another businesses, the process is called business-to-business
+  (B2B) online shopping. A typical online store enables the customer to browse the firm's range of products and 
+  services, view photos or images of the products, along with information about the product specifications, 
+  features and prices.
+
+Online stores usually enable shoppers to use "search" features to find specific models, brands or items.
+ Online customers must have access to the Internet and a valid method of payment in order to complete a 
+ transaction, such as a credit card, an Interac-enabled debit card, or a service such as PayPal. For physical
+  products (e.g., paperback books or clothes), the e-tailer ships the products to the customer; for digital 
+  products, such as digital audio files of songs or software, the e-tailer usually sends the file to the 
+  customer over the Internet. The largest of these online retailing corporations are Alibaba, Amazon.com, 
+  and eBay.[1] 
+  
+  NOUN (Person place or thing): Service, Customer, Product, Cart, Items
+  VERBS (Action word like Run): Find, Buy("AddToCart")
+  ADJECTIVE (Color, Shape  Size): Model, Brand, Price
+"""
 
 
-print(power(3))
+class Customer:
+    def __init__(self, name, number, order_history=[]):
+        self.name = name
+        self.number
+
+    def get_lifetime_value(self):
+        # return sum or all orders in history
+        return 0
+
+
+class Product:
+    def __init__(self, name, price, **props):
+        self.name = name
+        self.price = price
+        self.props = props  # size color
+
+
+class Store:
+    def __init__(self, name, products=[], customers=[], transactions=[], id=0):
+        pass
+
+
+class Transaction:
+    def __init__(self, product={name:"", price:0}, quantity=1, id=0):
+        pass
+
+
+class Cart:
+    def __init__(self, transactions=[], paymentMethod="VISA", card_number="", id=0):
+        pass
+
+    def total(self):
+        pass
+
+    def get_number_of_products(self):
+        pass
+
+
+if __name__ == "__main__":
+    s = Store("Amzone");
+    c = Customer("Bob")
+    products = [
+        Product("pencil", 1.00),
+        Product("paper", 2.00)
+    ]
+    
+    order = Cart()
+    
+    print(order.receipt()) # all customer info + all line items + total
+    
+    
